543c1f2c1b6a090a55a69c123041611e2ec694a3
Naeem <aicnaeem73@gmail.com>
2026-02-10 21:36:12 +0500

    NUCLEAR: Fix package.json JSON error, minimal configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-﻿/** @type {import("next").NextConfig} */
-const nextConfig = {
-  output: "export",
+﻿module.exports = {
+  output: "standalone",
   images: {
     unoptimized: true
-  },
-  // NO redirects - using _redirects file instead
-  trailingSlash: false
+  }
 }
-
-module.exports = nextConfig
